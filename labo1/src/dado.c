53fa27abf0547691d01a346efcef4329db8e5045
rogerdanielpiovetgarcia@gmail.com <rogerdanielpiovetgarcia@gmail.com>
2024-03-18 18:33:28 -0600

    test: Se probó cargar el firmware para encender únicamente el LED central de la cara del dado

---
+++ labo1/src/dado.c
@@ -0,0 +1,26 @@
+#include <pic14/pic12f675.h>
+
+void delay(unsigned int tiempo);
+
+void main(void)
+{
+    TRISIO = 0x00;      // Poner pines GPIO[5:0] (exceptuando GPIO3) como salidas
+    GPIO = 0x00;        // Poner todas las salidas en bajo 
+    
+    unsigned int time = 100;
+    
+    while (1)
+    {
+        GPIO = 0x10;
+        delay(time);
+    }
+}
+
+void delay(unsigned int tiempo)
+{
+    unsigned int i;
+    unsigned int j;
+
+    for(i = 0; i < tiempo; i++)
+        for(j = 0; j < 1275; j++);
+}
