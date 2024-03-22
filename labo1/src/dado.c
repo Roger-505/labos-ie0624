a0993dd4520662bbfcea2c6a06c2055b3f32b406
rogerdanielpiovetgarcia@gmail.com <rogerdanielpiovetgarcia@gmail.com>
2024-03-22 17:50:22 -0600

    fix: Por recomendación del profesor, se buscará un algoritmo RNG para generar números aleatorios, por lo que se eliminó la función randomADC, y además se agregó documentación al código

--- labo1/src/dado.c
+++ labo1/src/dado.c
@@ -1,37 +1,37 @@
 #include <pic14/pic12f675.h>
 
 void delay(unsigned int tiempo);
-int random(void);
 
 void main(void)
 {
-    TRISIO = (TRISIO << 6) | 0x01;          // Poner pines GPIO[5:4] y GPIO[2:0] como salidas, y GPIO3 como entrada
-    GPIO &= 0x00;                           // Poner GPIO[5:4] y GPIO[2:0] en bajo
+    // declaraciones de variables
+    int temp;   // variable aleatoria por implementar
     
-    unsigned int time = 100;
-    int temp = randomADC();
-
+    // inicialización de registros
+    TRISIO = (TRISIO << 6) | 0x01;  // Poner pines GPIO[5:4] y GPIO[2:0] como salidas, y GPIO3 como entrada
+    GPIO &= 0x00;                   // Poner GPIO[5:4] y GPIO[2:0] en bajo
+    
     while (1)
     {
         switch (temp)
         {
             case 1:
-                GPIO = 0x20;
+                GPIO = 0x20;    // mostrar un 1 en la cara del dado
                 break;
             case 2:
-                GPIO = 0x04;
+                GPIO = 0x04;    // mostrar un 2 en la cara del dado
                 break;
             case 3:
-                GPIO = 0x30;
+                GPIO = 0x30;    // mostrar un 3 en la cara del dado
                 break;
             case 4:
-                GPIO = 0x12;
+                GPIO = 0x12;    // mostrar un 4 en la cara del dado
                 break;
             case 5:
-                GPIO = 0x32;
+                GPIO = 0x32;    // mostrar un 5 en la cara del dado
                 break;
             case 6:
-                GPIO = 0x16;
+                GPIO = 0x16;    // mostrar un 6 en la cara del dado
                 break;
         }
     }
@@ -44,9 +44,4 @@ void delay(unsigned int tiempo)
 
     for(i = 0; i < tiempo; i++)
         for(j = 0; j < 1275; j++);
-}
-
-int randomADC(void)
-{
-    return 6;
 }
