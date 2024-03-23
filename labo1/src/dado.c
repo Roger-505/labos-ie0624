aa4e04c1f13f61c11670e15c98541ed6015dff77
rogerdanielpiovetgarcia@gmail.com <rogerdanielpiovetgarcia@gmail.com>
2024-03-22 18:19:46 -0600

    fix: Se cambió el enmascarado de GPIO, para contemplar que el pin 7 se utilizará como entrada GPIO3

--- labo1/src/dado.c
+++ labo1/src/dado.c
@@ -14,8 +14,8 @@ void main(void)
     int temp;   // variable aleatoria por implementar
     
     // inicialización de registros
-    TRISIO &= 0x00;                  // Poner pines GPIO[5:4] y GPIO[2:0] como salidas, y GPIO3 como entrada
-    GPIO &= 0x00;                   // Poner GPIO[5:4] y GPIO[2:0] en bajo
+    TRISIO &= 0x00;                 // Poner pines GPIO[5:4] y GPIO[2:0] como salidas. GPIO3 siempre se lee como 1
+    GPIO &= 0x08;                   // Poner GPIO[5:4] y GPIO[2:0] en bajo
     
     while (1)
     {
